b9678e1ac67caabe00298a22bd694add901f9ee4
BeWoler <bewoler.del@gmail.com>
2023-03-14 00:53:03 +0300

    finished postform

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <PostForm />
+  <PostForm @create="createPost" />
   <PostList :posts="posts" />
 </template>
 
@@ -8,6 +8,7 @@ import { defineComponent } from "vue";
 import "./index.css";
 import PostForm from "./components/PostForm.vue";
 import PostList from "./components/PostList.vue";
+import IPost from "./components/interfaces/post.interface";
 
 export default defineComponent({
   components: {
@@ -21,28 +22,11 @@ export default defineComponent({
         { id: 2, title: "12345", body: "Descr2" },
         { id: 3, title: "123666", body: "Descr3" },
       ],
-      title: "",
-      body: "",
     };
   },
   methods: {
-    createPost() {
-      const newPost = {
-        id: Date.now(),
-        title: this.title,
-        body: this.body,
-      };
-      this.posts.push(newPost);
-      this.title = "";
-      this.body = "";
-    },
-    inputTitle(event: Event) {
-      const { target } = event;
-      if (target) this.title = (target as HTMLInputElement).value;
-    },
-    inputBody(event: Event) {
-      const { target } = event;
-      if (target) this.body = (target as HTMLInputElement).value;
+    createPost(post: IPost) {
+      this.posts.push(post);
     },
   },
 });
--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -4,15 +4,13 @@
       type="text"
       placeholder="Title"
       class="border-2 border-black mr-4 px-1"
-      v-bind:value="title"
-      @input="inputTitle"
+      v-model="post.title"
     />
     <input
       type="text"
       placeholder="Description"
       class="border-2 border-black mr-4 px-1"
-      v-bind:value="body"
-      @input="inputBody"
+      v-model="post.body"
     />
     <button class="border-black border-2 bg-slate-100 p-1" @click="createPost">
       Create
@@ -23,5 +21,26 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 
-export default defineComponent({});
+export default defineComponent({
+  data() {
+    return {
+      post: {
+        id: 0,
+        title: "",
+        body: "",
+      },
+    };
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit("create", this.post);
+      this.post = {
+        id: 0,
+        title: "",
+        body: "",
+      };
+    },
+  },
+});
 </script>
